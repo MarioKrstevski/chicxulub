22b61ad8882ec9739e3a24edf9c078acf5bae686
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-07 17:05:07 +0200

    Lazy starting design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import SafetyIndicator from './components/SafetyIndicator'
+import React from "react";
+import SafetyIndicator from "./components/SafetyIndicator";
+// import PotentuallyHazardousAsteroids from "./components/PotentuallyHazardousAsteroids";
+// import FirstAsteroidThatHits from "./components/FirstAsteroidThatHits";
+// import BiggestSmallestFastestSlowest from "./components/BiggestSmallestFastestSlowest";
+// import BigAsteroidsList from "./components/SafetyIndiBigAsteroidsListcator";
+// import FastAsteroidsList from "./components/FastAsteroidsList";
 
 function App() {
+  const yearRange = 2;
+  const startDate = new Date();
+  const endDate = new Date(
+    startDate.getFullYear() + yearRange,
+    startDate.getMonth(),
+    startDate.getDate()
+  )
+
+  const startDateString = startDate.toISOString().slice(0, 10);
+  const endDateString = endDate.toISOString().slice(0, 10);
 
 
-  const startDate= new Date().toISOString()
-  const endDate = null
 
- console.log({startDate})
-
+  console.log({ startDateString, endDateString });
+  
   return (
-
-   <div> Asteroids app 
-   
-   <SafetyIndicator/>
-   </div>
+    <div>
+      Asteroids app
+      <SafetyIndicator />
+      {/* <PotentuallyHazardousAsteroids />
+      <FirstAsteroidThatHits />
+      <BiggestSmallestFastestSlowest />
+      <BigAsteroidsList />
+      <FastAsteroidsList /> */}
+    </div>
   );
 }
 
-export default App
+export default App;
