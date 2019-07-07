1411c07104f3470f2802b81cd52e8d162208d2b3
Mario Krstevski <mariokrstevski@hotmail.com>
2019-07-07 16:48:21 +0200

    First commit, starting on project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import SafetyIndicator from './components/SafetyIndicator'
 
 function App() {
+
+
+  const startDate= new Date().toISOString()
+  const endDate = null
+
+ console.log({startDate})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <div> Asteroids app 
+   
+   <SafetyIndicator/>
+   </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/SafetyIndicator.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export default function SafetyIndicator(){
+    return <div> I am a safety indicator hehe </div>
+}
